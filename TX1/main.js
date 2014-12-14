53b0ef2be3ad7d4ac2c56cc839c71b95b802db14
Vadym Yatsyuk <vadym.yatsyuk@innosabi.com>
2014-12-14 13:59:20 +0100

    removed comments

--- TX1/main.js
+++ TX1/main.js
@@ -2,55 +2,52 @@ var theThingsAPI = require('thethingsio-api');
 
 //create Client
 var client = theThingsAPI.createClient();
-
 var mraa = require('mraa');
-
 var button = new mraa.Gpio(2),
     rotationSensorPin = new mraa.Aio(0),
     lastRotation = 500, // center
-    touchSensor = new mraa.Gpio(4);
+    touchSensor = new mraa.Gpio(4),
+    jsonToSend = {};
 
 touchSensor.dir(mraa.DIR_IN);
 button.dir(mraa.DIR_IN);
-
 main();
 
 function main() {
-//    console.log('Touch: ' + touchSensor.read());
-    
-    var pressed =  button.read(); 
-//    console.log('Pressed: ' + pressed);
-    
-    var rotationAngle = rotationSensorPin.read();
-//    console.log('Rotation: ' + rotationAngle);
-    
-    var req3 = client.thingWrite({  
-        "values":
-            [
-                {
-                    "key": "motion",
-                    "value": (pressed === 1) ? "go" : "stop",
-                    "units": "",
-                    "type": "temporal"
-                },
-                {
-                    "key": "turn",
-                    "value" : rotation(rotationAngle),
-                    "units": "",
-                    "type": "temporal"
-                },
-                {
-                    "key": "light",
-                    "value": touchSensor.read(),
-                    "units": "",
-                    "type": "temporal"
-                }
-            ]
-    });
-    req3.on('response',function(res){
-        lastRotation = rotationAngle;
-    });
-    req3.end();
+    var pressed =  button.read(),
+        rotationAngle = rotationSensorPin.read(),
+        newJson = {
+            "values":
+                [
+                    {
+                        "key": "motion",
+                        "value": (pressed === 1) ? "go" : "stop",
+                        "units": "",
+                        "type": "temporal"
+                    },
+                    {
+                        "key": "turn",
+                        "value" : rotation(rotationAngle),
+                        "units": "",
+                        "type": "temporal"
+                    },
+                    {
+                        "key": "light",
+                        "value": touchSensor.read(),
+                        "units": "",
+                        "type": "temporal"
+                    }
+                ]
+        };
+
+    if (newJson !== jsonToSend) {
+        jsonToSend = newJson;
+        var req3 = client.thingWrite(jsonToSend);
+        req3.on('response',function(res) {
+            lastRotation = rotationAngle;
+        });
+        req3.end();
+    }
     
   setTimeout(main,400);
 }
